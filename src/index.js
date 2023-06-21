9369a3a692a9342bafe3aecd7ddbf4d81f316e1e
GAqua <98158025+GAqua@users.noreply.github.com>
2023-06-21 17:21:21 +0100

    base code

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", function(request, response) {
+    response.status(200);
+    response.send("online");
+    response.end();
+});
+
+app.listen(port);
